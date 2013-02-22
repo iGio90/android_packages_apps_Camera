e362b141a36d906d5c56bf560f162544f4e21e2f
jt1134 <jt1134@gmail.com>
2013-02-21 22:08:50 -0600

    HDR: only show slide animation after final shot
    Also make sure to reset mBurstShotsDone to 0 after taking the
    final shot for the HDR image.
    
    Change-Id: I5cb4d5286402b577e097d57aa4cc9d850a317aef

--- src/com/android/camera/PhotoModule.java
+++ src/com/android/camera/PhotoModule.java
@@ -968,7 +968,8 @@ public class PhotoModule
             // i.e. If monkey/a user swipes to the gallery during picture taking,
             // don't show animation
             if (ApiHelper.HAS_SURFACE_TEXTURE && !mIsImageCaptureIntent
-                    && mActivity.mShowCameraAppView) {
+                    && mActivity.mShowCameraAppView
+                    && !(mHDRShotInProgress && mBurstShotsDone < 3)) {
                 // Finish capture animation
                 ((CameraScreenNail) mActivity.mCameraScreenNail).animateSlide();
             }
@@ -1691,6 +1692,7 @@ public class PhotoModule
                 mHDRShotInProgress = false;
                 mHDRRendering = true;
                 mSnapshotOnIdle = false;
+                mBurstShotsDone = 0;
                 Log.d(TAG, "Done shooting all exposures, computing HDR");
 
                 // We release controls
